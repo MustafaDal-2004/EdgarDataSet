d04055b12b89f4357a7adf611582e1bd14dba5ab
Mustafa Dal <mustafadal2004@icloud.com>
2026-03-10 16:36:08 +0000

    Update Concept_List.py

--- Concept_List.py
+++ Concept_List.py
@@ -1,17 +1,397 @@
-# concept_list.py
-# -----------------------------
-# Driver concept keywords for 3-statement modeling
-
 concept_keywords = {
-    "Revenue": ["RevenueFromContractWithCustomerExcludingAssessedTax"],
-    "OperatingIncome": ["OperatingIncomeLoss"],
-    "NetIncome": ["NetIncomeLoss"],
-    "Capex": ["PropertyPlantAndEquipment", "Acquisitions", "CapitalExpenditures",
-              "PropertyPlantAndEquipmentNet", "PaymentsToAcquireProductiveAssets"],
-    "Depreciation": ["DepreciationDepletionAndAmortization", "Depreciation"],
-    "AccountsReceivable": ["AccountsReceivable", "Receivables"],
-    "Inventory": ["Inventory"],
-    "AccountsPayable": ["AccountsPayable", "Payables"],
-    "Cash": ["CashAndCashEquivalents", "CashCashEquivalents"],
-    "Debt": ["Debt", "NotesPayable", "LongTermDebt"]
+
+"Revenue": [
+"RevenueFromContractWithCustomerExcludingAssessedTax",
+"Revenues",
+"SalesRevenueNet",
+"SalesRevenueGoodsNet",
+"SalesRevenueServicesNet",
+"SalesRevenueServicesGross",
+"SalesRevenueGoodsGross",
+"NetSales",
+"NetSalesRevenue",
+"Revenue",
+"TotalRevenue",
+"TotalRevenues",
+"OperatingRevenue",
+"OperatingRevenues",
+"ContractRevenue",
+"ServiceRevenue",
+"ProductRevenue",
+"SubscriptionRevenue",
+"LicenseRevenue",
+"AdvertisingRevenue",
+"Sales",
+"SalesRevenue",
+"SalesRevenueGoods",
+"SalesRevenueServices",
+"RealEstateRevenueNet",
+"AdmissionsRevenue",
+"FranchiseRevenue",
+"RoyaltyRevenue",
+"RevenueMineralSales",
+"RevenueOilAndGas",
+"EnergyRevenue",
+"RetailRevenue",
+"OnlineStoreRevenue",
+"DigitalRevenue",
+"ConsultingRevenue",
+"CloudRevenue",
+"MaintenanceRevenue",
+"RentalRevenue",
+"LeasingRevenue",
+"TransportationRevenue",
+"BrokerageRevenue",
+"FinancialServicesRevenue",
+"InterestAndFeeIncomeLoans",
+"TradingRevenue",
+"InvestmentBankingRevenue"
+],
+
+"COGS": [
+"CostOfRevenue",
+"CostOfGoodsSold",
+"CostOfGoodsAndServicesSold",
+"CostOfServices",
+"CostOfSales",
+"CostOfRevenueGoods",
+"CostOfRevenueServices",
+"CostOfProductsSold",
+"CostOfProducts",
+"CostOfMerchandiseSold",
+"CostOfServicesRevenue",
+"CostOfSalesAndServices",
+"CostOfGoodsSoldExclusiveDepreciation",
+"CostOfSalesProduct",
+"CostOfSalesService",
+"CostOfMaterials",
+"CostOfProduction",
+"CostOfEnergySold",
+"CostOfRetailGoodsSold",
+"CostOfSoftwareLicenses",
+"CostOfHostingServices",
+"FulfillmentCosts",
+"LogisticsCosts",
+"DistributionCosts",
+"ManufacturingCosts"
+],
+
+"GrossProfit": [
+"GrossProfit",
+"GrossProfitLoss",
+"GrossMargin",
+"GrossIncome",
+"GrossOperatingProfit",
+"GrossProfitBeforeDepreciation",
+"GrossContribution",
+"GrossProfitFromOperations"
+],
+
+"SGA": [
+"SellingGeneralAndAdministrativeExpense",
+"SellingAndMarketingExpense",
+"GeneralAndAdministrativeExpense",
+"SellingExpense",
+"MarketingExpense",
+"AdvertisingExpense",
+"PromotionalExpense",
+"SalesAndMarketingExpense",
+"AdministrativeExpense",
+"CorporateGeneralAdministrativeExpense",
+"BusinessDevelopmentExpense",
+"DistributionExpense",
+"CustomerAcquisitionCost",
+"OfficeExpense",
+"ProfessionalFees",
+"LegalExpense",
+"ConsultingExpense",
+"ManagementFeeExpense",
+"OperatingExpenseAdministrative"
+],
+
+"R&D": [
+"ResearchAndDevelopmentExpense",
+"ResearchDevelopmentExpense",
+"ProductDevelopmentExpense",
+"SoftwareDevelopmentExpense",
+"TechnologyDevelopmentExpense",
+"InnovationExpense",
+"EngineeringExpense",
+"ResearchExpense",
+"DevelopmentExpense",
+"ClinicalTrialExpense"
+],
+
+"OperatingIncome": [
+"OperatingIncomeLoss",
+"OperatingIncome",
+"IncomeFromOperations",
+"IncomeLossFromOperations",
+"OperatingProfit",
+"OperatingEarnings",
+"EarningsFromOperations",
+"OperatingProfitLoss",
+"OperatingIncomeBeforeDepreciation",
+"OperatingIncomeAfterDepreciation",
+"OperatingMarginIncome",
+"IncomeFromContinuingOperationsBeforeInterestAndTaxes",
+"EBIT"
+],
+
+"InterestExpense": [
+"InterestExpense",
+"InterestExpenseDebt",
+"InterestExpenseBorrowings",
+"InterestAndDebtExpense",
+"InterestExpenseLongTermDebt",
+"InterestExpenseShortTermBorrowings",
+"InterestExpenseCapitalLease",
+"InterestExpenseNotesPayable",
+"InterestExpenseBankLoans",
+"InterestExpenseConvertibleDebt",
+"InterestExpenseMortgage",
+"InterestExpenseOther"
+],
+
+"IncomeTax": [
+"IncomeTaxExpenseBenefit",
+"IncomeTaxExpense",
+"ProvisionForIncomeTaxes",
+"CurrentIncomeTaxExpense",
+"DeferredIncomeTaxExpense",
+"IncomeTaxProvision",
+"IncomeTaxesPaid",
+"IncomeTaxBenefit",
+"IncomeTaxExpenseContinuingOperations"
+],
+
+"NetIncome": [
+"NetIncomeLoss",
+"ProfitLoss",
+"NetIncome",
+"NetEarnings",
+"NetIncomeAvailableToCommonStockholdersBasic",
+"NetIncomeAvailableToCommonStockholdersDiluted",
+"NetIncomeAttributableToParent",
+"NetIncomeContinuingOperations",
+"NetIncomeCommonStockholders",
+"NetIncomeLossAvailableToCommonStockholdersBasic",
+"NetIncomeLossAvailableToCommonStockholdersDiluted",
+"ComprehensiveIncomeNetOfTax",
+"NetIncomeBeforeExtraordinaryItems",
+"IncomeAvailableToCommonStockholders"
+],
+
+"Cash": [
+"CashAndCashEquivalentsAtCarryingValue",
+"CashCashEquivalentsAndShortTermInvestments",
+"CashAndCashEquivalents",
+"Cash",
+"CashEquivalents",
+"RestrictedCashAndCashEquivalents",
+"CashAndDueFromBanks",
+"CashAndDeposits",
+"CashAndShortTermInvestments",
+"CashInBank",
+"PettyCash",
+"RestrictedCash",
+"CashCollateral"
+],
+
+"AccountsReceivable": [
+"AccountsReceivableNetCurrent",
+"AccountsReceivableCurrent",
+"AccountsReceivable",
+"ReceivablesNetCurrent",
+"TradeAccountsReceivableNet",
+"Receivables",
+"CustomerReceivables",
+"AccountsNotesReceivable",
+"AccountsReceivableGross",
+"TradeReceivables",
+"NotesReceivable",
+"LoansReceivable",
+"InterestReceivable",
+"ReceivablesFromCustomers",
+"ContractReceivable"
+],
+
+"Inventory": [
+"InventoryNet",
+"InventoryFinishedGoods",
+"InventoryRawMaterials",
+"InventoryWorkInProcess",
+"Inventories",
+"InventoryGross",
+"MerchandiseInventory",
+"InventoryGoods",
+"InventoryTotal",
+"RawMaterialsInventory",
+"FinishedGoodsInventory",
+"WorkInProcessInventory",
+"SuppliesInventory",
+"PartsInventory"
+],
+
+"OtherCurrentAssets": [
+"PrepaidExpenseCurrent",
+"OtherAssetsCurrent",
+"OtherCurrentAssets",
+"PrepaidInsurance",
+"PrepaidRent",
+"DeferredCostsCurrent",
+"ContractAssetsCurrent",
+"DerivativeAssetsCurrent",
+"IncomeTaxesReceivable",
+"OtherReceivablesCurrent"
+],
+
+"CurrentAssets": [
+"AssetsCurrent",
+"TotalCurrentAssets",
+"CurrentAssets",
+"AssetsHeldForSaleCurrent",
+"CurrentFinancialAssets"
+],
+
+"AccountsPayable": [
+"AccountsPayableCurrent",
+"AccountsPayable",
+"TradeAccountsPayable",
+"AccountsPayableTrade",
+"AccountsPayableAndAccruedLiabilities",
+"Payables",
+"AccountsPayableAndOtherCurrentLiabilities",
+"TradePayables",
+"SupplierPayables",
+"VendorPayables"
+],
+
+"OtherCurrentLiabilities": [
+"AccruedLiabilitiesCurrent",
+"OtherLiabilitiesCurrent",
+"OtherCurrentLiabilities",
+"DeferredRevenueCurrent",
+"ContractLiabilityCurrent",
+"AccruedPayroll",
+"AccruedTaxes",
+"AccruedInterest",
+"CustomerDeposits",
+"WarrantyLiabilityCurrent"
+],
+
+"CurrentLiabilities": [
+"LiabilitiesCurrent",
+"TotalCurrentLiabilities",
+"CurrentLiabilities",
+"CurrentFinancialLiabilities"
+],
+
+"Debt": [
+"LongTermDebt",
+"LongTermDebtNoncurrent",
+"DebtCurrent",
+"ShortTermBorrowings",
+"LongTermDebtAndCapitalLeaseObligations",
+"NotesPayable",
+"NotesPayableCurrent",
+"Debt",
+"Borrowings",
+"TotalDebt",
+"LongTermBorrowings",
+"ShortTermDebt",
+"BankDebt",
+"ConvertibleDebt",
+"SeniorNotes",
+"SubordinatedDebt",
+"TermLoan",
+"RevolvingCreditFacility",
+"MortgageDebt",
+"CapitalLeaseObligations"
+],
+
+"Equity": [
+"StockholdersEquity",
+"StockholdersEquityIncludingPortionAttributableToNoncontrollingInterest",
+"TotalEquity",
+"CommonStockEquity",
+"PreferredStockEquity",
+"RetainedEarnings",
+"AccumulatedOtherComprehensiveIncomeLoss",
+"AdditionalPaidInCapital",
+"TreasuryStock",
+"NoncontrollingInterest",
+"EquityAttributableToParent"
+],
+
+"Depreciation": [
+"Depreciation",
+"DepreciationAndAmortization",
+"DepreciationDepletionAndAmortization",
+"DepreciationAmortizationAndAccretionNet",
+"DepreciationExpense",
+"DepreciationAndAmortizationExpense",
+"AmortizationOfIntangibleAssets",
+"DepreciationDepletion",
+"DepreciationExpensePropertyPlantEquipment",
+"AmortizationExpense"
+],
+
+"StockComp": [
+"ShareBasedCompensation",
+"StockBasedCompensation",
+"ShareBasedCompensationExpense",
+"EquityCompensationExpense",
+"StockOptionExpense",
+"RestrictedStockExpense",
+"EmployeeStockCompensation"
+],
+
+"Capex": [
+"PaymentsToAcquireProductiveAssets",
+"CapitalExpenditures",
+"PurchaseOfPropertyPlantAndEquipment",
+"PaymentsForPropertyPlantAndEquipment",
+"PaymentsToAcquirePropertyPlantAndEquipment",
+"PaymentsToAcquireBuildings",
+"PaymentsToAcquireMachineryEquipment",
+"PaymentsToAcquireSoftware",
+"PaymentsForCapitalImprovements",
+"PaymentsToAcquireIntangibleAssets",
+"PaymentsToAcquireOtherProductiveAssets",
+"CapitalSpending",
+"Capex"
+],
+
+"ChangeWorkingCapital": [
+"IncreaseDecreaseInAccountsReceivable",
+"IncreaseDecreaseInInventory",
+"IncreaseDecreaseInAccountsPayable",
+"IncreaseDecreaseInOperatingAssetsAndLiabilities",
+"ChangeInWorkingCapital",
+"IncreaseDecreaseInOtherCurrentAssets",
+"IncreaseDecreaseInOtherCurrentLiabilities",
+"ChangeInOperatingWorkingCapital"
+],
+
+"Dividends": [
+"PaymentsOfDividends",
+"DividendsPaid",
+"CommonStockDividendsPerShareDeclared",
+"Dividends",
+"CashDividendsPaid",
+"PreferredStockDividends",
+"DividendsDeclared"
+],
+
+"SharesOutstanding": [
+"WeightedAverageNumberOfSharesOutstandingBasic",
+"WeightedAverageNumberOfSharesOutstandingDiluted",
+"CommonStockSharesOutstanding",
+"CommonSharesOutstanding",
+"SharesOutstanding",
+"WeightedAverageShares",
+"DilutedShares"
+]
+
 }
